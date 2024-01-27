e289b846aabe7eb4fd4df2084e721960bcedc79a
Amir Sadiev <amir.sadiev@gmail.com>
2024-01-28 03:09:24 +0600

    used products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,40 +3,15 @@
 
 //creating an array, bc it represents a list. And we have a list of different products. 
 //We will use an object to represent each individual product (bc an obj lets us group multiple values(name,img,price) together)
-let products = [{
-  img: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    countReview: 87
-  },
-  priceCents: 1090
-  // calculate in cents, b/c JS has a problem with decimal points (floats) so the best practice for calculating money, is to calculate it in cents. We also made sure the save the value of 'priceCents' instead of just 'price'.
-}, {
-  img: "images/products/intermediate-composite-basketball.jpg",
-  name: "Intermediate Size Basketball",
-  rating: {
-    stars: 4,
-    countReview: 127
-  },
-  priceCents: 2095
-}, {
-  img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating: {
-    stars: 4.5,
-    countReview: 56
-  },
-  priceCents: 799
-}];
 
 let accumulatedHTML = '';
 
+//how does this amazon.js file know abt the products array from the products.js file? 
 products.forEach(e => {
   accumulatedHTML += `<div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
-          src=${e.img}>
+          src=${e.image}>
       </div>
 
       <div class="product-name limit-text-to-2-lines">
@@ -47,7 +22,7 @@ products.forEach(e => {
         <img class="product-rating-stars"
           src="images/ratings/rating-${e.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-          ${e.rating.countReview}
+          ${e.rating.count}
         </div>
       </div>
 
