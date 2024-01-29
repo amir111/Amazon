d24ca79634011ae05f1c62ab4139b2ab13ce563a
Amir Sadiev <amir.sadiev@gmail.com>
2024-01-30 05:51:50 +0600

    display cart qty interactively

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -52,12 +52,12 @@ products.forEach(e => {
         Added
       </div>
 
-      <button 
-
-      class="add-to-cart-button 
+      <button class="
+      add-to-cart-button 
       button-primary
       js-add-to-cart-btn"
       
+      data-atrb-id="${e.id}"
       data-atrb-name="${e.name}"
       >
         Add to Cart
@@ -76,15 +76,16 @@ document.querySelectorAll('.js-add-to-cart-btn').forEach((eBtn) => {
 
     //console.log(eBtn.dataset.atrbName) if we clicked the basketball "add to cart" button, this console log will show "Intermediate Size Basketball"
     //.dataset property gives us all the data attributes attached to this btn elt
-    //.atrb-name is converted to atrbName so we use that converted style
+    //.atrb-name in kebab-style is converted to atrbName camelCase style so we use that converted style. And actually, if it was ".atrb-Name", it would be converted to camelCase "atrbname".
 
+    let prodID = eBtn.dataset.atrbId;
     let prodName = eBtn.dataset.atrbName;
 
-    let repeatedItem = false; 
+    let repeatedItem = false;
     let i; //declared outside of forEach so, that it can be used again
 
     cart.forEach((item) => {
-      if (prodName === item.productName) { //does it exist in the cart?
+      if (prodID === item.id) { //does it exist in the cart?
         repeatedItem = true; //if so, then define repeatedItem as current item
         i = item; //store item in 'i' for later use
         // console.log(item)
@@ -95,11 +96,23 @@ document.querySelectorAll('.js-add-to-cart-btn').forEach((eBtn) => {
       i.quantity++; //if we just use item.quantity++, it'll say item not defined
     } else { //else create that item in the cart
       cart.push({
+        id: prodID,
         productName: prodName,
         quantity: 1
       })
     }
-    
+
+    //just added this to display total amount of stuff in the cart
+    let totalItemsInCart = 0;
+    let qt;
+    cart.forEach(item => {
+      qt = item.quantity;
+      totalItemsInCart += qt;
+    })
+
     console.log(cart)
+    console.log("Total quantity of stuff in cart: " + totalItemsInCart)
+
+    document.querySelector(".js-cart-qty").innerHTML = totalItemsInCart;
   })
 })
