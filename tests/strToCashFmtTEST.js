a0b379acd3f7850a69fb9fb02e4c7f4b973096ce
Amir Sadiev <amir.sadiev@gmail.com>
2024-04-13 17:42:43 +0500

    created automated tests in strToCashFmtTEST.js file to test strToCashFormat.js file

--- tests/strToCashFmtTEST.js
+++ tests/strToCashFmtTEST.js
@@ -3,8 +3,12 @@
 
 import { convertToCashFormat } from '../scripts/utils/strToCashFormat.js'
 
+
+console.log('Test Suite: Converting Str to Cash Format {') //TITLE of Test SUITE 1 (group of related Tests)
+
 //Testing a specific situation. "Situation" same thing as saying "test case". 
 // Test case 1 (basic)
+console.log('- Converts cents to dollars:'); //TITLE of Test case 1
 if (convertToCashFormat(2099) === '20.99') {
   console.log('passed');
 } else {
@@ -12,6 +16,7 @@ if (convertToCashFormat(2099) === '20.99') {
 }
 
 // add another test, (test case 2) (edge case 1)
+console.log('- Works with just 0:'); //TITLE of Test case 2 (edge case 1) 
 if (convertToCashFormat(0) === '0.00') {
   console.log('passed');
 } else {
@@ -19,6 +24,7 @@ if (convertToCashFormat(0) === '0.00') {
 }
 
 //Test case 3 (edge case 2)
+console.log('- Rounds up correctly to the nearest cent:'); //TITLE of Test case 3 (edge case 2) 
 if (convertToCashFormat(2000.5) === '20.01') {
   console.log('passed');
 } else {
@@ -29,6 +35,7 @@ if (convertToCashFormat(2000.5) === '20.01') {
 //A: Basic test cases and edge cases (if edge cases exist). Normal input, and edge cases (tricky situations). 
 
 //Test case 4 (edge case 3)
+console.log('- Rounds down correctly to the nearest cent:'); //TITLE of Test case 4 (edge case 3) 
 if (convertToCashFormat(2000.4) === '20.00') {
   console.log('passed TC4');
 } else {
