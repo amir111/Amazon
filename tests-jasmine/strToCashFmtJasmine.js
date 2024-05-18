93322775e1a0c8d934094df931a6ab8dd4316db2
Amir Sadiev <amir.sadiev@gmail.com>
2024-05-18 17:56:24 +0500

    jasmine testing framework practice

---
+++ tests-jasmine/strToCashFmtJasmine.js
@@ -0,0 +1,12 @@
+import { convertToCashFormat } from "../scripts/utils/strToCashFormat.js";
+
+// describe() is a keyword provided by Jasmine. 
+// It is a fnc that creates a test suite (test group)
+// the first arg is a string, and it the name of test group (test suite)
+// the sec arg is a function that we want it to do 
+describe('Test Suite 1: Converting Str to Cash Format', () => {
+  // inside here we will create a test, and give the test a nm
+  // to create a new single test in Jasmine, we use another fnc provided by Jasmine, called it()
+  // the first param of it() is the test name (also called spec name)
+  it('Converts cents to dollars:');
+});
