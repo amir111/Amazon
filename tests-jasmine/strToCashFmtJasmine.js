32319cc552025e76b7310d363096be9f88eb3706
Amir Sadiev <amir.sadiev@gmail.com>
2024-05-21 16:32:36 +0500

    retested tests, but now using Jasmine keyword functions like 'describe()', 'it()' and 'expect()'

--- tests-jasmine/strToCashFmtJasmine.js
+++ tests-jasmine/strToCashFmtJasmine.js
@@ -8,5 +8,15 @@ describe('Test Suite 1: Converting Str to Cash Format', () => {
   // inside here we will create a test, and give the test a nm
   // to create a new single test in Jasmine, we use another fnc provided by Jasmine, called it()
   // the first param of it() is the test name (also called spec name)
-  it('Converts cents to dollars:');
+  it('Converts cents to dollars', () => {
+    expect(convertToCashFormat('2095')).toEqual('20.95')
+  });
+
+  it('Works with just zero, (edge case N1)', () => {
+    expect(convertToCashFormat('0')).toEqual('0.00')
+  })
+
+  it('Rounds up correctly to the nearest cent, (edge case N2) ', () => {
+    expect(convertToCashFormat(2000.5)).toEqual('20.01')
+  })
 });
